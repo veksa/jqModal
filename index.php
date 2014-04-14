2966a6c9d30a943c434fb311d6e61930870194f4
Alex Khijnij <veksa@aspro.ru>
2014-04-14 17:22:09 +0600

    initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+	<head>
+		<title>example jqModal</title>
+		<link href="/css/style.css" type="text/css" rel="stylesheet" />
+		
+		<script type="text/javascript" src="/js/jquery.js"></script>
+		<script type="text/javascript" src="/js/jqModal.js"></script>
+		<script type="text/javascript" src="/js/general.js"></script>
+	</head>
+	<body>
+		<div class="container">
+			Обычное всплывающее окно:
+			<span class="link" data-event="jqm" data-name="example" data-param-id="1" data-autoload-text1="test data input" data-autohide="location.reload();">Вызов всплывающего окна</span>
+			<br/><br/>
+			Всплывающее окно с прокруткой:
+			<span class="link" data-event="jqm" data-name="example2" data-param-id="2" data-scroll="Y">Вызов всплывающего окна</span>
+		</div>
+	</body>
+</html>
---
+++ js/general.js
@@ -0,0 +1,97 @@
+function onLoadjqm( hash ){
+	var name = hash.t.className.split(/[\s\!,\.\?]+/g).pop();
+	
+	$.each( $(hash.t).get(0).attributes, function( index, attr ){
+		if( /^data\-autoload\-(.+)$/.test( attr.nodeName ) ){
+			var key = attr.nodeName.match(/^data\-autoload\-(.+)$/)[1];
+			if( $('.jqmWindow [name="'+key+'"]').is('select') ){
+				$('select[name="'+key+'"]').find('#'+$(hash.t).data('autoload-'+key)).attr('selected', 'selected');
+			}else if( $('.jqmWindow [name="'+key+'"]').is('input') ){
+				$('input[name="'+key+'"]').val( $(hash.t).data('autoload-'+key) ).attr('readonly', 'readonly');
+			}else{
+				console.log('onLoadjqm: warning, not defined type');
+			}
+		}
+	});
+	
+	if( $(hash.t).data('autohide') ){
+		$(hash.w).data('autohide', $(hash.t).data('autohide'));
+	}
+	
+	if( $(hash.t).data('scroll') ){
+		if( $(hash.w).parent().hasClass('jqmWindowOut') ){
+			$(hash.w).parent().show();
+		}else{
+			$(hash.w).wrap('<div class="jqmWindowOut"></div>');
+		}
+		$('body').css('overflow', 'hidden').css('paddingRight', '15px');
+		
+		$('.jqmWindowOut').on('click', function(e){
+			$(this).find('.jqmClose').click();
+		});
+		
+		$('.jqmWindowOut').find('.jqmWindow').on('click', function(e){
+			e.stopPropagation();          
+		});
+	}
+	
+	hash.w.addClass('show').css({ 'margin-left': '-' + hash.w.width() / 2+'px', 'top': $(document).scrollTop() + ( $(window).height() - hash.w.height() ) / 2 + 'px', 'opacity': 1 });
+	setTimeout( function(){ $('body').scrollTop(); }, 300 );
+}
+
+function onHide( hash ){
+	if( $(hash.w).data('autohide') ){
+		eval( $(hash.w).data('autohide') );
+	}
+	
+	if( $(hash.t).data('scroll') ){
+		$('.jqmWindowOut').hide();
+		$('body').css('overflow', 'scroll').css('paddingRight', '0');
+	}
+	
+	hash.w.css('opacity', 0).hide();
+	hash.w.empty();
+	hash.o.remove();
+	hash.w.removeClass('show');
+}
+ 
+// Функция вызова
+$.fn.jqmEx = function( options ){
+	if( !$(this).length ){
+		return this;
+	}
+	
+	$(this).each(function(){
+		var _this = $(this);
+		var name = _this.data('name');
+		var script = "/ajax/form.php";
+		$.each( _this.get(0).attributes, function( index, attr ){
+			if( /^data\-param\-(.+)$/.test( attr.nodeName ) ){
+				var key = attr.nodeName.match(/^data\-param\-(.+)$/)[1];
+				if( script == "/ajax/form.php" ){
+					script += '?';
+				}else{
+					script += '&';
+				}
+				
+				script += key + '=' + _this.data('param-'+key);
+			}
+		});
+		
+		if( $('.'+name+'_frame').length > 0 ){
+			return;
+		}
+		if( $('.button[data-name="'+name+'"]' ).attr('disabled') != 'disabled' ){
+			$('body').find('.'+name+'_frame').remove();
+			$('body').append('<div class="'+name+'_frame jqmWindow" style="width: 574px"></div>');
+			
+			$('.'+name+'_frame').jqm({trigger: '*[data-name="'+name+'"]', onLoad: function( hash ){ onLoadjqm( hash ); }, onHide: function(hash){ onHide( hash ); }, ajax: script });
+		}
+	});
+	
+	return this;
+}
+
+$(document).ready(function(){
+	$('*[data-event="jqm"]').jqmEx();
+})
